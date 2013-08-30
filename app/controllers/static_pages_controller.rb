b00e35948253eb87c762ac7addfbb09fd6a0f130
Lemuel <lemueluhuru@gmail.com>
2013-08-30 19:46:04 -0400

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    if signed_in?
+      @micropost = current_user.microposts.build if signed_in?
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      #similar to t he 'new' action within the signup form
+      #current_user.microposts.build created a new micropost
+      #thats blank, this allows the form_for method to make the
+      #approriate request upon submission.
+    end
   end
 
   def help
